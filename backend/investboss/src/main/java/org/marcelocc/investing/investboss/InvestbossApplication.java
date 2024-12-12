6853d601279ee268a7c9f60a732c66e65b103190
mccaetano <marcelo.cheruti@gmail.com>
2024-12-12 09:59:04 -0300

    first commit

---
+++ backend/investboss/src/main/java/org/marcelocc/investing/investboss/InvestbossApplication.java
@@ -0,0 +1,13 @@
+package org.marcelocc.investing.investboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestbossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestbossApplication.class, args);
+	}
+
+}
---
+++ backend/investboss/src/test/java/org/marcelocc/investing/investboss/InvestbossApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marcelocc.investing.investboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestbossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
